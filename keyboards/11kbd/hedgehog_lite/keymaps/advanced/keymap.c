84b4bbca3b5531b78c5f220ff1a03a1ff8c7f992
Max Elfner <maximilian.elfner@kit.edu>
2022-11-11 15:46:54 +0100

    Added many features to hedgehog advanced keymap

--- keyboards/11kbd/hedgehog_lite/keymaps/advanced/keymap.c
+++ keyboards/11kbd/hedgehog_lite/keymaps/advanced/keymap.c
@@ -10,23 +10,114 @@ enum layers{
   _FN
 };
 
+enum custom_keycodes {
+    MAC_LOCK = SAFE_RANGE
+};
+
+// Lighting layers
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 15, MY_RED}
+);
+const rgblight_segment_t PROGMEM my_capsword_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 15, MY_BLUE}
+);
+const rgblight_segment_t PROGMEM my_mac_fn_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {0, 8, MY_CHAT}
+);
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_mac_fn_layer,
+    my_capsword_layer,
+    my_capslock_layer,
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+// and enable callbacks
+bool led_update_user(led_t led_state) {
+    rgblight_set_layer_state(2, led_state.caps_lock);
+    return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    rgblight_set_layer_state(0, layer_state_cmp(state, _FN));
+    return state;
+}
+
 // Keymaps
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BASE] = LAYOUT(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_GRV,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, TG(1),
-        KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_DQUO, KC_ENT,
+        KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_SPC, KC_RGUI, MO(1), MO(1)
+        KC_LCTL, KC_LGUI, KC_SPC, KC_RGUI, MO(1), KC_LEAD
     ),
 
     [_FN] = LAYOUT(
-        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, RGB_VAD, RGB_VAI, ____, ____,
-        RGB_TOG, ____, KC_UP, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, TG(1),
+        MAC_LOCK, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, RGB_VAD, RGB_VAI, ____, ____,
+        RGB_TOG, ____, KC_UP, ____, ____, ____, ____, ____, ____, ____, ____, RGB_MODE_REVERSE, RGB_MODE_FORWARD, ____, ____,
         ____, KC_LEFT, KC_DOWN, KC_RIGHT, ____, ____, ____, ____, ____, ____, ____, ____, ____,
         ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
-        ____, ____, ____, ____, MO(1), MO(1)
+        ____, ____, ____, ____, ____, ____
     ),
 
 };
+
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
+
+        // Caps word when using double leader
+        SEQ_ONE_KEY(KC_LEAD) {
+            caps_word_on();
+            rgblight_set_layer_state(1, true);
+        }
+
+        // caps lock with triple leader
+        SEQ_TWO_KEYS(KC_LEAD, KC_LEAD) {
+            tap_code(KC_CAPS);
+        }
+
+        // more basic replace macros
+        SEQ_TWO_KEYS(KC_S, KC_C) {
+            send_string("[sic!]");
+        }
+
+    }
+}
+
+// disable caps word light
+void caps_word_set_user(bool active) {
+    if (!active) {
+      rgblight_set_layer_state(1, false);
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+      case MAC_LOCK:
+          if (record->event.pressed) {
+              register_code(KC_LCTL);
+              register_code(KC_LGUI);
+              register_code(KC_Q);
+          } else {
+              unregister_code(KC_LCTL);
+              unregister_code(KC_LGUI);
+              unregister_code(KC_Q);
+          }
+          return false;
+
+      default:
+          return true;
+    }
+}
