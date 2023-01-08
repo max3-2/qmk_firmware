6fcf2c27498178b7f01091bb7be08d6ec8c3db65
Max Elfner <maximilian.elfner@kit.edu>
2023-01-08 20:27:09 +0100

    hedghehog: switch layer and leader keys

--- keyboards/11kbd/hedgehog_lite/keymaps/advanced/keymap.c
+++ keyboards/11kbd/hedgehog_lite/keymaps/advanced/keymap.c
@@ -1,6 +1,3 @@
-// Copyright 2022 CMM.Studio Freather
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 
 #define ____ KC_TRNS
@@ -57,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, TG(1),
         KC_LALT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LGUI, KC_SPC, KC_RGUI, MO(1), KC_LEAD
+        KC_LCTL, KC_LGUI, KC_SPC, KC_RGUI, KC_LEAD, MO(1)
     ),
 
     [_FN] = LAYOUT(
@@ -67,7 +64,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ____, ____, ____, ____, ____, ____, ____, ____, ____, RGB_HUD, RGB_HUI, ____,
         ____, ____, ____, ____, ____, ____
     ),
-
 };
 
 LEADER_EXTERNS();
