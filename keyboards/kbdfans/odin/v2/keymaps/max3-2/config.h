0bbb3dca7fa82e7af4a6af8edd86c174c42fdd88
Max Elfner <maximilian.elfner@kit.edu>
2023-04-08 11:19:05 +0200

    Odin v2: Improved LED handling

--- keyboards/kbdfans/odin/v2/keymaps/max3-2/config.h
+++ keyboards/kbdfans/odin/v2/keymaps/max3-2/config.h
@@ -7,6 +7,19 @@
 
 #ifdef RGB_DI_PIN
     #define RGBLIGHT_LAYERS_RETAIN_VAL
+    #undef RGBLIGHT_EFFECT_SNAKE
+    #undef RGBLIGHT_EFFECT_CHRISTMAS
+    #undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#endif
+
+// More light
+#ifdef RGBLIGHT_VAL_STEP
+    #undef RGBLIGHT_VAL_STEP
+    #define RGBLIGHT_VAL_STEP 10
+#endif
+#ifdef RGBLIGHT_LIMIT_VAL
+    #undef RGBLIGHT_LIMIT_VAL
+    #define RGBLIGHT_LIMIT_VAL 200
 #endif
 
 // Leader config
